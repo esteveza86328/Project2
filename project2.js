27cbe5ae648b7a6e9ea9173ad9965a02b48595e4
Avery Estevez <99348095+esteveza86328@users.noreply.github.com>
2024-10-19 11:02:13 -0400

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,29 +1,31 @@
 let counter=0;
 var tasks = [];
-const task=new Object();
-let taskname;
-let taskstatus;
-let taskprio;
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector(".submit").onclick = submit;
 })
 
 function submit(){
-    let taskname=document.getElementById("taskname").value;
+  event.preventDefault();
+  let taskname;
+  let taskstatus;
+  let taskprio;
+    taskname=document.getElementById("taskname").value;
     console.log(taskname);
     taskprio=document.getElementById("selectprio").value;
     console.log(taskprio);
     const radioButtons = document.querySelectorAll('input[name="taskradio"]');
     radioButtons.forEach(radioButton => {
       if (radioButton.checked) {
-      taskstatus = radioButton.value;
-      console.log(taskstatus);
+        taskstatus = radioButton.value;
+        console.log(taskstatus);
       }
     });
-    task.taskname=taskname;
-    task.taskstatus=taskstatus;
-    task.taskprio=taskprio;
+    const task = {
+      taskname: taskname,
+      taskstatus: taskstatus,
+      taskprio: taskprio
+  };
     tasks.push(task);
     const node = document.createElement("li");
     node.id=counter;
@@ -32,25 +34,24 @@ function submit(){
     const prionode = document.createTextNode(" Trask priority: " + taskprio);
     const doneButton = document.createElement("button");
     doneButton.textContent = "Mark done";
-    doneButton.class="done";
     doneButton.addEventListener("click", () => {
-        const listItem = event.target.parentElement;
-        listItem.style.textDecoration = "line-through";
-      });
+      const listItem = event.target.parentElement;
+      listItem.style.textDecoration = "line-through";
+    });
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.addEventListener("click", () => {
-        delete task[counter];
-        const listItem = event.target.parentElement;
-        listItem.remove();
-      });
+      delete tasks[counter];
+      const listItem = event.target.parentElement;
+      listItem.remove();
+    });
     node.appendChild(namenode);
     node.appendChild(prionode);
     node.appendChild(statusnode);
     node.appendChild(doneButton);
     node.appendChild(deleteButton);
 
-    document.getElementById("tasklist").appendChild(node);
     counter++;
     document.getElementById('taskname').value ="";
+    document.getElementById("tasklist").appendChild(node);
 }
